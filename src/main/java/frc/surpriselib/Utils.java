5cb43dba6b3ee4c9d2e699348e03d72578e1b982
DeltaDizzy <EJBraswell@gmail.com>
2022-02-04 00:28:21 -0600

    mathutils->utils + add neutralmode->boolean method

--- src/main/java/frc/surpriselib/Utils.java
+++ src/main/java/frc/surpriselib/Utils.java
@@ -4,8 +4,23 @@
 
 package frc.surpriselib;
 
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+
 /** Add your docs here. */
-public class MathUtils {
+public class Utils {
+
+    /**
+     * @return True if <code>NeutralMode.Brake</code>, False otherwise
+     */
+    public static boolean neutralModeIsBrake(NeutralMode mode) {
+        if (mode == NeutralMode.Brake) {
+            return true;
+        } else if (mode == NeutralMode.Coast) {
+            return false;
+        }
+        return false;
+    }
+
     public static double clamp(double value)
     {
         return clamp(value, 1.0, -1.0);
@@ -46,21 +61,23 @@ public class MathUtils {
         }
     }
 
-    public static double[] normalize(double[] motorSpeeds)
+    public static double[] normalize(double[] values)
     { 
-        double max = Math.abs(motorSpeeds[0]);
+        double max = Math.abs(values[0]);
         boolean normFlag = max > 1; // do we normalize or just pass it back?
-        for (int i = 1; i < motorSpeeds.length; i++) {
-            if (Math.abs(motorSpeeds[i]) > max) { // set max to biggest magnitude motor speed
-                max = Math.abs(motorSpeeds[i]);
+        for (int i = 1; i < values.length; i++) {
+            if (Math.abs(values[i]) > max) { // set max to biggest magnitude motor speed
+                max = Math.abs(values[i]);
                 normFlag = max > 1;
             }
         }
         if (normFlag) { // normalize speeds
-            for (int i = 0; i < motorSpeeds.length; i++) {
-                motorSpeeds[i] /= max;
+            for (int i = 0; i < values.length; i++) {
+                values[i] /= max;
             }
         }
-        return motorSpeeds;
+        return values;
     }
+
+
 }
